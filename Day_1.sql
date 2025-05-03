d6d70aa35b74cfad32a145fa9d9ceda02634a21a
Aphiwe Makwelo <72697191+A-MAK15@users.noreply.github.com>
2025-05-03 10:54:03 +0200

    Create Day_1.sql

---
+++ Day_1.sql
@@ -0,0 +1,11 @@
+-- Day1
+SELECT 
+SELECT first_name, last_name FROM actor;
+
+DISTINCT
+SELECT DISTINCT (rental_rate) FROM film;
+
+SELECT DISTINCT rating FROM film;
+
+COUNT
+SELECT COUNT (DISTINCT amount) FROM payment;
