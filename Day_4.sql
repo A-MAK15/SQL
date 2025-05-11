da8ccd70bdb5193d8524613201f14c02224f637e
Aphiwe Makwelo <72697191+A-MAK15@users.noreply.github.com>
2025-05-11 09:26:00 +0200

    Update Day_4.sql

--- Day_4.sql
+++ Day_4.sql
@@ -1,1 +1,10 @@
+-- Day_4
+-- GROUP BY
+SELECT staff_id, customer_id, SUM(amount) FROM payment
+GROUP BY customer_id, staff_id
+ORDER BY customer_id, staff_id;
+
+SELECT DATE(payment_date), SUM(amount) FROM payment
+GROUP BY DATE(payment_date)
+ORDER BY SUM(amount) DESC;
 
