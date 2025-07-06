25268b71bceb27930e323865a0e4fd2abf2e475d
Aphiwe Makwelo <72697191+A-MAK15@users.noreply.github.com>
2025-07-06 14:23:21 +0200

    Update Day_9.sql

--- Day_9.sql
+++ Day_9.sql
@@ -1,1 +1,21 @@
+-- EXTRACT
+SELECT EXTRACT(MONTH FROM payment_date)
+FROM payment
 
+-- AGE
+SELECT AGE(payment_date)
+FROM payment
+
+-- TOCHAR
+SELECT TO_CHAR(payment_date, 'MONTH')
+FROM payment
+
+-- Challenge
+-- Task 1
+SELECT DISTINCT(TO_CHAR(payment_date, 'MONTH'))
+FROM payment
+
+-- Task 2
+SELECT COUNT(*)
+FROM payment
+WHERE EXTRACT(DOW FROM payment_date) = 1
