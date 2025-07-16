7313849b63e007a3b9e184e97b0a3e5331e4c17b
Aphiwe Makwelo <72697191+A-MAK15@users.noreply.github.com>
2025-07-16 20:41:00 +0200

    Update Day_16.sql

--- Day_16.sql
+++ Day_16.sql
@@ -24,3 +24,24 @@ FROM depts
 
 DELETE FROM depts 
 WHERE department = 'B'
+
+
+-- VIEWS
+CREATE VIEW customer_info AS
+SELECT first_name, last_name, address FROM customer
+INNER JOIN address
+ON customer.address_id = address.address_id
+
+SELECT * FROM customer_info
+
+CREATE OR REPLACE VIEW customer_info AS
+SELECT first_name, last_name, address, district FROM customer
+INNER JOIN address
+ON customer.address_id = address.address_id
+
+DROP VIEW IF EXIST customer_info
+
+ALTER VIEW customer_info RENAME to c_info
+
+SELECT * FROM c_info
+
